fe4258a40ec311c66161a92501526ad51673742b
BigGuy00 <darkcarravenger@yahoo.com>
2015-06-04 20:59:20 -0400

    we now have three levels
    i split up the one level into three levels

---
+++ Grimoire-master/Assets/Scripts/nextLevel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class nextLevel : MonoBehaviour {
+
+	public int levelToGoTo;
+
+	void OnTriggerStay(Collider other)
+	{
+		if(other.name == "NextLevel")
+		{
+			Application.LoadLevel (levelToGoTo);
+		}
+	}
+}
