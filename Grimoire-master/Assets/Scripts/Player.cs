2763bebc3643891d0346c74d18c3d2e200c2f1a4
alexmcary <alexmcary@gmail.com>
2015-06-12 17:55:41 -0400

    Animations for Casting are Working
    Added an animation for Asterdimon throwing spells.

--- Grimoire-master/Assets/Scripts/Player.cs
+++ Grimoire-master/Assets/Scripts/Player.cs
@@ -5,6 +5,7 @@ public class Player : MonoBehaviour {
 
 	Animator anim;
 	private float speed;
+	private float cast;
 
 	// Use this for initialization
 	void Start () {
@@ -14,11 +15,21 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		speed = move.moveSpeed;
+	 
 		if (speed > 0) {
 			anim.SetFloat ("Walk", 1);
 		}
 		if (speed < 1) {
 			anim.SetFloat ("Walk", 0);
 		}
-	}
+		if (Input.GetKey ("1") || Input.GetKey ("2") || Input.GetKey ("3")) {
+			cast = 0.2f;
+			anim.SetFloat ("Cast", cast);
+		} 
+		else {
+			cast = 0.0f;
+			anim.SetFloat ("Cast", cast);
+		}
+	
+    }
 }
